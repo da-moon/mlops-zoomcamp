ce96677e6209110338d23640b0a55b0c8da10f63
da-moon <damoon.azarpazhooh@badal.io>
2024-05-29 17:54:16 -0400

    feat(02-experiment-tracking): q5
    Signed-off-by: da-moon <damoon.azarpazhooh@badal.io>

--- cohorts/2024/02-experiment-tracking/homework/hpo.py
+++ cohorts/2024/02-experiment-tracking/homework/hpo.py
@@ -10,6 +10,7 @@ from sklearn.metrics import mean_squared_error
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("random-forest-hyperopt")
+mlflow.autolog(disable=True)
 
 
 def load_pickle(filename: str):
@@ -35,12 +36,14 @@ def run_optimization(data_path: str, num_trials: int):
 
     def objective(params):
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
-
-        return {'loss': rmse, 'status': STATUS_OK}
+        with mlflow.start_run():
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric("rmse",rmse)
+            return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
