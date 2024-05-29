ae375e9af6d02574e9de96ab5c5470330f2da5f3
da-moon <damoon.azarpazhooh@badal.io>
2024-05-29 16:33:18 -0400

    feat: q2
    Signed-off-by: da-moon <damoon.azarpazhooh@badal.io>

--- cohorts/2024/02-experiment-tracking/homework/train.py
+++ cohorts/2024/02-experiment-tracking/homework/train.py
@@ -3,7 +3,8 @@ import pickle
 import click
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+import mlflow
 
 
 def load_pickle(filename: str):
@@ -18,16 +19,25 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
+    train_data_path=os.path.join(data_path, "train.pkl")
+    mlflow.log_param("train-data-path", train_data_path)
+    X_train, y_train = load_pickle(train_data_path)
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    valid_data_path=os.path.join(data_path, "val.pkl")
+    mlflow.log_param("valid-data-path", valid_data_path)
+
+    X_val, y_val = load_pickle(valid_data_path)
 
     rf = RandomForestRegressor(max_depth=10, random_state=0)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
-
+    rmse = root_mean_squared_error(y_val, y_pred)
+    mlflow.log_metric("rmse", rmse)
 
 if __name__ == '__main__':
-    run_train()
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment")
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "da-moon")
+        run_train()
